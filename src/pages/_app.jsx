ce44b88d78c354494309b66a59293dfc2e5d30ab
agoodman1999 <agoodman@civiconnect.ca>
2023-09-18 10:25:22 -0400

    initial commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { SessionProvider } from "next-auth/react";
+
+config.autoAddCss = false;
+
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}) {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
+}
