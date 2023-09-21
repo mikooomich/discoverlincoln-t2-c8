99805778a8bcfb9592600b6f80eb5940cde4f2c3
agoodman1999 <agoodman@civiconnect.ca>
2023-09-21 09:34:06 -0400

    fix incorrect global styles import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,9 +11,6 @@ import { useSession, signIn } from "next-auth/react";
 import { signOut } from "next-auth/react";
 import axios from "axios";
 
-import "../styles/globals.css";
-import "../styles/test.css";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
