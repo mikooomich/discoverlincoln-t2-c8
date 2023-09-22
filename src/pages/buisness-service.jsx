511d7e43c2457255f473c2be7474dbc736e36335
MikoooMichael <mikooomichael@gmail.com>
2023-09-22 13:40:01 -0400

    Create businesses and services page
    I realize that it is better to continue after the components get done

---
+++ src/pages/buisness-service.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import DefaultButton from '@/components/DefaultButton'
+
+
+export default function checkout() {
+  return (
+        <>
+        <style jsx>
+        {`
+        #buisnessPsudoBody {
+                        margin: auto;
+                        max-width: 1200px;
+                }
+           
+        
+        `}
+
+
+
+        </style>
+       
+
+        <div id="buisnessPsudoBody">
+             <div className='selectionsContent'>
+                {/* placeholder, steal the ones used on home page and modify it? */}
+
+             </div>
+
+
+             <div className='cardStash'>
+
+                        
+             </div>
+        </div>
+        </>
+
+  )
+}
