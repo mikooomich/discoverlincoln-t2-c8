12020ae2541f403528e7810ffa2e5be2013ed39e
MikoooMichael <mikooomichael@gmail.com>
2023-09-22 14:53:28 -0400

    Create reusable button component Fixes #67

--- src/components/DefaultButton.jsx
+++ src/components/DefaultButton.jsx
@@ -1,28 +1,37 @@
-
-
 import React from 'react'
 
-export default function DefaultButton({ color, fontSize, text}) {
-	if (text == undefined) {
-		text = "Placeholder";
-	}
-
+/**
+ * Defaults:
+ * text colour = white
+ * background colour = --color-topographic-green
+ * font size = --font-size-body-L
+ * padding = --padding-btn-default
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
+export default function DefaultButton({isLink = false, bgColor = "var(--color-topographic-green)", 
+textColor = "#FFFFFF", fontSize = "var(--font-size-body-L)", padding = "var(--padding-btn-default)", className = "", href = "", children}) {
 	return (
 		<>
-		<button>{text}</button>
-	
+		{isLink && <a href= {href} className={className}>{children}</a>}
+		{!isLink && <button className={className}>{children}</button>}
+		
+		
 		<style jsx>
 			{`
 			button {
-				background-color: var(--color-topographic-green);
-				font-size: var(--font-size-body-L);
-				padding: var(--padding-btn-default);
-				color: #FFFFFF;
+				background-color: ${bgColor};
+				font-size: ${fontSize};
+				padding: ${padding};
+				color: ${textColor};
 				font-family: var(--font-calps);
 			}
-			`}</style>
+
+			a {
+				{/* TODO: Style the links */}
+			}
+		`}</style>
 		</>
 	)
-
-	
 }
