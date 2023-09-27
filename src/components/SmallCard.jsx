4a64a37964713533a64c7678b81602d5f3f772dd
MikoooMichael <mikooomichael@gmail.com>
2023-09-27 13:34:50 -0400

    create map item card component Fixes #63

---
+++ src/components/SmallCard.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow, faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+
+/**
+ * Children is the icon you want to put under the title. 
+ * You will control the size, Recommended size 20x20
+ * 
+ * imgUrl: the url of the image of the event or what not ex. /local.svg or https://......
+ * @param {*} param0 
+ * @returns 
+ */
+export default function SmallCard({
+	title = "Title wha wah",
+	category = "restaurant",
+	imgUrl = "https://travelforfoodhub.com/wp-content/uploads/2023/05/Best-Wine-Regions-in-Europe.jpg",
+	children
+}) {
+
+	const categoryColors = {
+		restaurant: "orange",
+		music: "purple",
+	};
+
+
+	return (
+		<>
+			<div className="small-card-mobile">
+				<div className="image-container"></div>
+
+				<div className="card-information-wrap">
+					<div className="large-info-wrap">
+						<div className="large-info-wrap-left">
+							<h1 className="title">{title}</h1>
+						</div>
+						
+						<div className="large-info-wrap-right">
+
+							<div className="title-wrap">
+								<div className="icon">
+									{children}
+								</div>
+								<div className="category-tag">
+									<h1>Placeholder</h1>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+
+
+				<div className="action-icon-wrap">
+
+					<div>
+						<FontAwesomeIcon icon={faLocationArrow} />
+					</div>
+
+					<div>
+						<FontAwesomeIcon icon={faArrowUpFromBracket} />
+
+					</div>
+
+				</div>
+			</div>
+
+			<style jsx>{`	
+
+			.small-card-mobile {
+				display: flex;
+				align-items: center;
+
+				max-width: 360px;
+				height: 90px;
+
+				box-shadow: var(--shadow-box-massive-card);
+			}
+
+			.image-container {
+				width: 160px;
+				height: 65px;
+				background-position: center center;
+				background-repeat: no-repeat;
+				background-image: url(${imgUrl});
+				background-size: cover;
+				border-radius: 5px;
+				border: 1px solid #B7B7B7;
+				margin-left: 10px;
+			}
+
+			.card-information-wrap {
+				display: flex;
+				flex-direction: column;
+				padding: 10px 15px;
+				width:100%
+			}
+
+	
+
+			.category-tag {
+				border-radius: var(--border-radius-pill);
+				color: white;
+				background-color: ${categoryColors[category]};
+				padding: 5px;
+				font-weight: 500;
+				font-family: var(--font-roboto);
+				font-size: var(--font-size-body-S);
+				width: fit-content;
+			}
+
+			.title {
+				font-family: var(--font-calps);
+				font-size: var(--font-size-body-L);
+				font-weight: 500;
+			}
+
+
+			.action-icon-wrap {
+				align-self: center;
+				justify-self: end;
+				
+			}
+			.action-icon-wrap div {
+				margin: 0px 10px 10px 0px;
+				color: ${categoryColors[category]};
+			}
+
+			.title-wrap {
+				display: flex;
+				align-items: center;
+			}
+
+			.icon {
+				margin-right: 10px;
+			}
+
+			
+		`}</style>
+		</>
+	);
+}
