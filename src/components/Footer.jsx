4d1619a733c0df4eb6b6d4a5c46d8050077ee7be
Sanjae Suresh <sanjaesuresh@gmail.com>
2023-09-27 16:05:40 -0400

    responsive footer Fixes #77

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,52 @@
-import React from 'react'
-import TextInput from './TextInput'
-import Image from "next/image"
+import React from "react";
+import TextInput from "./TextInput";
+import Image from "next/image";
 
 export default function Footer() {
-	//code
+  //code
 
-	//html
-	return (
-		<>
-			{/* html */}
-			<div className="footer">
-				<div className="footer-logos">
-					<Image src="footerIcons.svg" width={100} height={100} alt="Discover Lincon on Facebook, Instagram, Twitter"
-						style={"max-width: 10px;"}
-					/>
-				</div>
-				<div className="footer-links">
-					<a href="./homepage">Home</a>
-					<a href="./search">Search</a>
-					<a href="./events-attractions">Upcoming Events</a>
-					<a href="./events-attractions">Attractions</a>
-					<a href="./business-service">Businesses & Services</a>
-					<a href="./profile">Profile</a>
-				</div>
-				<div className="footer-subscription">
-					<h2 className="subscribe-text">Subscribe to Town of Lincoln</h2>
-					<div className="subscription-inputs">
-						<TextInput type="text" className="email-input" placeholder="Enter email"></TextInput>
-						<button type="button" className="button-style">Submit</button>
-					</div>
-				</div>
-			</div>
+  //html
+  return (
+    <>
+      {/* html */}
+      <div className="footer">
+        <div className="footer-logos">
+          <Image
+            src="footerIcons.svg"
+            width={100}
+            height={100}
+            alt="Discover Lincon on Facebook, Instagram, Twitter"
+            style={"max-width: 10px;"}
+          />
+        </div>
+        <div className="footer-links">
+          <a href="./homepage">Home</a>
+          <a href="./search">Search</a>
+          <a href="./events-attractions">Upcoming Events</a>
+          <a href="./events-attractions">Attractions</a>
+          <a href="./business-service">Businesses & Services</a>
+          <a href="./profile">Profile</a>
+        </div>
+        <div className="footer-subscription">
+          <h2 className="subscribe-text">Subscribe to Town of Lincoln</h2>
+          <div className="subscription-inputs">
+            <TextInput
+              type="text"
+              className="email-input"
+              placeholder="Enter email"
+            ></TextInput>
+            <button type="button" className="button-style">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
 
-			{/* styles */}
-			<style jsx>{`
+      {/* styles */}
+      <style jsx>{`
+
+		
+
         .footer {
           display: flex;
           flex-direction: row;
@@ -50,24 +63,32 @@ export default function Footer() {
         .logo {
           float: left;
           width: 300px;
-          display: center;
         }
 
-	.footer-logos {
-		display: flex;
-		justify-content: center;
-		margin-left: 20px;
-	}
+        .footer-logos {
+          display: flex;
+          justify-content: center;
+          margin-left: 20px;
+		  flex-shrink: 0;
+		  max-width: 100%;
+        }
 
         .footer-wrap {
           display: flex;
           flex-direction: row;
+          justify-content: center;
+          margin: auto;
+		  flex-wrap: wrap;
+
         }
 
         .footer-links {
-          display: flex;
+          display: inline-flex;
           flex-direction: row;
           align-items: center;
+          flex-wrap: wrap;
+		  margin-left: 180px;
+		  
         }
 
         .footer-links a {
@@ -75,6 +96,10 @@ export default function Footer() {
           color: white;
           padding: 10px;
           font-size: 15px;
+		  line-height: 5px;
+		  font-family: var(--font-roboto);
+		  font-size: var(--font-size-body-Mplus);
+		  
         }
 
         .footer-links a:hover {
@@ -86,37 +111,63 @@ export default function Footer() {
         .footer-subscription {
           display: flex;
           flex-direction: column;
-	  align-self: center;
+          align-self: center;
         }
 
+		@media only screen and (max-width: 1019px){
+			.footer-subscription{ 
+				display: none; 
+			}
+		}
+
+		@media only screen and (max-width: 1207px){
+			.footer-links{ 
+				margin-left: 0px; 
+			}
+		}
+
+		@media only screen and (max-width: 725px){
+			.footer-links a{
+				font-size: var(--font-size-body-M);
+			}
+			.footer{
+				padding-left: 5px;
+				padding-right: 5px;
+			}
+		}
+ 
         .subscription-inputs {
           display: flex;
           flex-direction: row;
           padding: 5px;
           gap: 5px;
+		  flex-wrap: wrap;
         }
-
-                    .subscribe-text{
-                        font-weight: bold;
-                    }
-                    .button-style{
-                        display: flex;
-                        width: 81px;
-                        height: 30px;
-                        padding: 3px 15px;
-                        gap: 10px;
-                        flex-shrink: 0;
-                        background: var(--TOPOGRAPHIC-GREEN, #00A62E);
-                        backdrop-filter: blur(2px);
-                        color: white;
-                    }
 
         .subscribe-text {
-          color: white;
+          font-weight: bold;
+		  font-family: var(--font-roboto);
+		  font-size: var(--font-size-body-L);
+		  color: white;
           padding: 5px;
           font-size: 15px;
+
         }
+        .button-style {
+          display: flex;
+          width: 81px;
+          height: 30px;
+          padding: 3px 15px;
+          gap: 10px;
+          flex-shrink: 0;
+          background: var(--TOPOGRAPHIC-GREEN, #00a62e);
+          backdrop-filter: blur(2px);
+          color: white;
+		  font-family: var(--font-roboto);
+        }
+
+        
       `}</style>
-		</>
-	);
+    </>
+  );
 }
